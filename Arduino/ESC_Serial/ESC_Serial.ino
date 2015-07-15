738daede0086f6ca59163edf2f5fe6dbc836d122
Debner Anton <debnera1@niksula.hut.fi>
2015-07-15 11:38:47 +0300

    Added methods for detaching and attaching. Freeform input.
    User can attach motor with 'a' and detach with 'd'.
    Input is now accepted in range 0-180.

--- Arduino/ESC_Serial/ESC_Serial.ino
+++ Arduino/ESC_Serial/ESC_Serial.ino
@@ -1,18 +1,29 @@
-#include <Servo.h> 
+#include <Servo.h>
 
 #define MIN_SIGNAL 700 // Default 544us (Typical ESC-range: 700us-2000us)
 #define MAX_SIGNAL 2000  // Default 2400us
 #define MOTOR_PIN 3
 
-Servo myservo;
-int curSpeed;
-int offset = 0;
+Servo myMotor;
+int speed = 0;
+String incomingString;
 
 
 void setup()
 {
   Serial.begin(9600);
-  myservo.attach(MOTOR_PIN, MIN_SIGNAL, MAX_SIGNAL); // Attach pin, set min and max pwm range
+  speed = 0; // Set default speed
+  Serial.println("Starting speed set to 0");
+  Serial.println("Motor offline, waiting for input - Attach with 'a', detach with 'd'");
+}
+
+void setSpeed(int angle) {
+  myMotor.write(angle);
+}
+
+void attachMotor()
+{
+  myMotor.attach(MOTOR_PIN, MIN_SIGNAL, MAX_SIGNAL); // Attach pin, set min and max pwm range
   Serial.println("Motor attached");
   String a = "PWM range is set to ";
   a += MIN_SIGNAL;
@@ -20,51 +31,77 @@ void setup()
   a += MAX_SIGNAL;
   a += "uS";
   Serial.println(a);
-  curSpeed = -1;
+  myMotor.write(speed);
 }
 
-void setSpeed(int angle){
-  myservo.write(angle);    
+void detachMotor()
+{
+  myMotor.detach();
+  Serial.println("Motor detached");
 }
-
 
 void loop()
 {
-  if (Serial.available())
+  // If there is incoming value
+  if (Serial.available() > 0)
   {
-    char c = Serial.read();
-    if('0' <= c && c <= '9')
+    // read the value
+    char ch = Serial.read();
+    if (ch == 'a' && Serial.peek() == '\n')
     {
-      curSpeed = (c - '0')*10 + offset;
-      Serial.print("Speed set to ");
-      Serial.println(curSpeed);
+      attachMotor();
+      incomingString = "";
     }
-    else if(c == 'q')
+    if (ch == 'd' && Serial.peek() == '\n')
     {
-      offset = 0;
-      Serial.print("Offset set to ");
-      Serial.println(offset);
+      detachMotor();
+      incomingString = "";
     }
-    else if(c == 'w')
+    if (ch != 10) { // 10 equals to newline
+      Serial.print("I have received: ");
+      Serial.print(ch, DEC);
+      Serial.print('\n');
+      incomingString += ch;
+    }
+    else if (incomingString.length() > 0) 
     {
-      offset = 90;
-      Serial.print("Offset set to ");
-      Serial.println(offset);
+      // print the incoming string
+      Serial.println("I am printing the entire string");
+      Serial.println(incomingString);
+
+      // Convert the string to an integer
+      int val = incomingString.toInt();
+
+      // print the integer
+      Serial.println("Printing the value: ");
+      Serial.println(val);
+
+      /*
+      *  We only want to write an integer between
+      *  0 and 180 to the motor.
+      */
+      if (val > -1 && val < 181)
+      {
+        Serial.println("Value is between 0 and 180");
+        // Set the speed and write it to the motor, if motor is attached.
+        speed = val;
+        if (myMotor.attached() == true)
+        {
+          myMotor.write(val);
+        }
+        else
+        {
+          Serial.println("Motor is not attached, attach by sending 'a' - starting speed changed to given value");
+        }
+      }
+      else
+      {
+        Serial.println("Value is NOT between 0 and 180");
+        Serial.println("Error with the input");
+      }
+
+      // Reset the value of the incomingString
+      incomingString = "";
     }
-    else if(c == 's')
-    {
-      curSpeed = -1;
-      Serial.print("Speed set to -1  --> Motor disabled");
-      Serial.println(curSpeed);
-    }
-    else
-    {
-      Serial.println("Unknown command");
-    }
-    
-  }
-  if(0 <= curSpeed && curSpeed >= 180)
-  {
-    setSpeed(curSpeed);  
   }
 }
