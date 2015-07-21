dc9753c7e4fd8043eaf477a366909af69b44161b
Debner Anton <debnera1@niksula.hut.fi>
2015-07-21 13:38:05 +0300

    Added capability for multiple motors. Started redoing input parsing

--- Arduino/ESC_Serial/ESC_Serial.ino
+++ Arduino/ESC_Serial/ESC_Serial.ino
@@ -1,43 +1,100 @@
 #include <Servo.h>
+#include "Motor.h"
 
 #define MIN_SIGNAL 700 // Default 544us (Typical ESC-range: 700us-2000us)
 #define MAX_SIGNAL 2000  // Default 2400us
-#define MOTOR_PIN 3
+#define MOTOR1_PIN 3
+#define MOTOR2_PIN 5
+#define MOTOR3_PIN 6
+#define MOTOR4_PIN 9
 
-Servo myMotor;
+
+
+Motor motor1 = {MOTOR1_PIN, "Motor 1"};
+Motor motor2 = {MOTOR2_PIN, "Motor 2"};
+Motor motor3 = {MOTOR3_PIN, "Motor 3"};
+Motor motor4 = {MOTOR4_PIN, "Motor 4"};
+Motor motors[4] = {motor1, motor2, motor3, motor4};
+
 int speed = 0;
-String incomingString;
 
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
   speed = 0; // Set default speed
   Serial.println("Starting speed set to 0");
-  Serial.println("Motor offline, waiting for input - Attach with 'a', detach with 'd'");
-}
-
-void setSpeed(int angle) {
-  myMotor.write(angle);
-}
-
-void attachMotor()
-{
-  myMotor.attach(MOTOR_PIN, MIN_SIGNAL, MAX_SIGNAL); // Attach pin, set min and max pwm range
-  Serial.println("Motor attached");
   String a = "PWM range is set to ";
   a += MIN_SIGNAL;
   a += "uS - ";
   a += MAX_SIGNAL;
   a += "uS";
   Serial.println(a);
-  myMotor.write(speed);
+  Serial.println("Motors are offline, waiting for input - Attach with 'a', detach with 'd'");
 }
 
-void detachMotor()
+void setSpeed(Motor motor, int angle) 
 {
-  myMotor.detach();
-  Serial.println("Motor detached");
+  if (motor.servo.attached() == true)
+  {
+    motor.servo.write(angle);
+  }
+    
+}
+
+void attachMotor(Motor motor)
+{
+  if (motor.servo.attached() == false)
+  {
+    motor.servo.attach(motor.pin, MIN_SIGNAL, MAX_SIGNAL); // Attach pin, set min and max pwm range
+    Serial.print(motor.description);
+    Serial.println(" attached");
+    setSpeed(motor, speed); 
+  }
+}
+
+void detachMotor(Motor motor)
+{
+  if (motor.servo.attached() == false)
+  {
+    motor.servo.detach();
+    Serial.print(motor.description);
+    Serial.println(" detached");
+  }
+}
+
+String readSerial()
+{
+  unsigned int maxDelay = 50000; // 50ms
+  unsigned int minDelay = 100; // 0.5ms
+  unsigned int timeDelayed = 0;
+  String input;
+  char c = 0;
+  // Incase we have multiple commands in buffer (separated with '\n'), return the first command found.
+  while (timeDelayed < maxDelay)
+  {
+    while (Serial.available() > 0)
+    {
+      c = Serial.read();
+      input += c;
+    }
+    if (input.endsWith(String('\n')) == true) // End of command received
+    {
+      break;
+    }
+    delayMicroseconds(minDelay); // Wait for all the data to be received.
+    timeDelayed += minDelay;
+  }
+  Serial.print("Delay: ");
+  Serial.println(timeDelayed);
+  
+  return input;
+}
+
+void parseThrottle(String input)
+{
+  int value = input.toInt();
+  Serial.println(value);
 }
 
 void loop()
@@ -45,63 +102,10 @@ void loop()
   // If there is incoming value
   if (Serial.available() > 0)
   {
-    // read the value
-    char ch = Serial.read();
-    if (ch == 'a' && Serial.peek() == '\n')
-    {
-      attachMotor();
-      incomingString = "";
-    }
-    if (ch == 'd' && Serial.peek() == '\n')
-    {
-      detachMotor();
-      incomingString = "";
-    }
-    if (ch != 10) { // 10 equals to newline
-      Serial.print("I have received: ");
-      Serial.print(ch, DEC);
-      Serial.print('\n');
-      incomingString += ch;
-    }
-    else if (incomingString.length() > 0) 
-    {
-      // print the incoming string
-      Serial.println("I am printing the entire string");
-      Serial.println(incomingString);
-
-      // Convert the string to an integer
-      int val = incomingString.toInt();
-
-      // print the integer
-      Serial.println("Printing the value: ");
-      Serial.println(val);
-
-      /*
-      *  We only want to write an integer between
-      *  0 and 180 to the motor.
-      */
-      if (val > -1 && val < 181)
-      {
-        Serial.println("Value is between 0 and 180");
-        // Set the speed and write it to the motor, if motor is attached.
-        speed = val;
-        if (myMotor.attached() == true)
-        {
-          myMotor.write(val);
-        }
-        else
-        {
-          Serial.println("Motor is not attached, attach by sending 'a' - starting speed changed to given value");
-        }
-      }
-      else
-      {
-        Serial.println("Value is NOT between 0 and 180");
-        Serial.println("Error with the input");
-      }
-
-      // Reset the value of the incomingString
-      incomingString = "";
-    }
+    String command = readSerial();
+    Serial.println("received:");
+    Serial.print(command);
+    Serial.print('\n');
+    //parseThrottle(command);
   }
 }
