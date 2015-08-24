c084f0e57ee5e14992b50b48af795ebf6056489a
Anton Debner <anton.debner@gmail.com>
2015-08-24 14:41:35 +0300

    Empty placeholder

---
+++ Arduino/FlightController/FlightController.ino
@@ -0,0 +1,16 @@
+/*
+ Name:		FlightController.ino
+ Created:	8/24/2015 1:37:29 PM
+ Author:	Anton
+*/
+
+// the setup function runs once when you press reset or power the board
+#include "serial_communicator.h"
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
