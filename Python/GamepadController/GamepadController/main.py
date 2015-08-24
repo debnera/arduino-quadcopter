71d7e6277391ee2dd8dcefc7a6ada15bfe5475f0
Anton Debner <anton.debner@gmail.com>
2015-08-24 11:16:14 +0300

    Initial commit

---
+++ Python/GamepadController/GamepadController/main.py
@@ -0,0 +1,15 @@
+﻿""" Created by Anton Debner in 2015 """
+from serialcommunicator import SerialCommunicator
+from angles import Angles
+from consoleinput import ConsoleInput
+from controllerinput import ControllerInput
+
+serialCom = SerialCommunicator(38400, "port")
+console = ConsoleInput(func1, exit)
+controller = ControllerInput(0)
+targetAngles = Angles(0.0, 0.0, 0.0)
+
+def exit():
+    serialCom.writeNow("exit\n")
+    serialCom.close()
+    controller.close()
