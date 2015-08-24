bbc681ed4b1af0fde1ed472c1129431660edbaf9
Anton Debner <anton.debner@gmail.com>
2015-08-24 12:03:24 +0300

    Added basic functions.

--- Python/GamepadController/GamepadController/main.py
+++ Python/GamepadController/GamepadController/main.py
@@ -4,12 +4,34 @@ from angles import Angles
 from consoleinput import ConsoleInput
 from controllerinput import ControllerInput
 
-serialCom = SerialCommunicator(38400, "port")
-console = ConsoleInput(func1, exit)
-controller = ControllerInput(0)
-targetAngles = Angles(0.0, 0.0, 0.0)
+class Main(object):
 
-def exit():
-    serialCom.writeNow("exit\n")
-    serialCom.close()
-    controller.close()
+    def __init__(self):
+        self.baudrate = 38400
+        self.port = "port"
+        self.serialCom = SerialCommunicator(self.baudrate, self.port)
+        self.controller = ControllerInput(0)
+        self.console = ConsoleInput(self.write, self.exit, self.restart)
+        self.console.start()
+        self.targetAngles = Angles(0.0, 0.0, 0.0)
+
+    def write(self,str):
+        self.serialCom.addToWriteBuffer(str)
+
+    def restart(self):
+        print("Restarting")
+        self.serialCom.close()
+        self.controller.close()
+        self.serialCom = SerialCommunicator(self.baudrate, self.port)
+        self.controller = ControllerInput(0)
+
+    def exit(self):
+        print("Exiting")
+        self.serialCom.writeNow("exit\n")
+        self.serialCom.close()
+        self.controller.close()
+
+
+if __name__ == "__main__":
+    main = Main()
+    
