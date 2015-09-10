728e7d49ab45d12da2f4beabf2c2fd9276bed0b4
Anton Debner <anton.debner@gmail.com>
2015-09-10 12:19:21 +0300

    Changed cont variables to static const.

--- Arduino/FlightController/angles.h
+++ Arduino/FlightController/angles.h
@@ -11,17 +11,21 @@ Author:		Anton
 #else
 	#include "WProgram.h"
 #endif
+
+
 class Angles
 {
 private:
-	const int kMaxDecimals = 3;
-	const float kInvalidNumber = -999;
+	static const int kMaxDecimals = 3;
+	static const int kInvalidNumber = -999;
+
 public:
 	float yaw;
 	float pitch;
 	float roll;
 
 	Angles(float yaw = 0, float pitch = 0, float roll = 0);
+	void setValues(float yaw, float pitch, float roll);
 	String toString();
 	bool fromString(String str);
 	float stringToFloat(String str, int from, int to);
