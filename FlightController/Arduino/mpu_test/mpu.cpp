24a41566eac92dc2d0e3d5d903b21accd562920d
Anton Debner <anton.debner@gmail.com>
2015-12-16 15:25:54 +0200

    Added a function for printing ypr

--- FlightController/Arduino/mpu_test/mpu.cpp
+++ FlightController/Arduino/mpu_test/mpu.cpp
@@ -89,8 +89,46 @@ bool MPU::init()
 
 Angles MPU::getAngles()
 {
-	// TODO - Implement this function.
 	if (!mpuInterrupt && fifoCount < packetSize) return Angles(0, 0, 0);
+	mpuInterrupt = false;
+    mpuIntStatus = mpu.getIntStatus();
+
+    // get current FIFO count
+    fifoCount = mpu.getFIFOCount();
+
+    // check for overflow (this should never happen unless our code is too inefficient)
+    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+        // reset so we can continue cleanly
+        mpu.resetFIFO();
+        Serial.println(F("FIFO overflow!"));
+
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+    } 
+	else if (mpuIntStatus & 0x02) {
+        // wait for correct available data length, should be a VERY short wait
+        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+        // read a packet from FIFO
+        mpu.getFIFOBytes(fifoBuffer, packetSize);
+        
+        // track FIFO count here in case there is > 1 packet available
+        // (this lets us immediately read more without waiting for an interrupt)
+        fifoCount -= packetSize;
+
+		// display Euler angles in degrees
+		mpu.dmpGetQuaternion(&q, fifoBuffer);
+		mpu.dmpGetGravity(&gravity, &q);
+		mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+		Serial.print("ypr\t");
+		Serial.print(ypr[0] * 180/M_PI);
+		Serial.print("\t");
+		Serial.print(ypr[1] * 180/M_PI);
+		Serial.print("\t");
+		Serial.println(ypr[2] * 180/M_PI);
+		
+	}
+
+	return Angles(0, 0, 0);
 	
 }
 
--- FlightController/Arduino/mpu_test/mpu.h
+++ FlightController/Arduino/mpu_test/mpu.h
@@ -30,6 +30,16 @@ private:
 	uint8_t fifoBuffer[64]; // FIFO storage buffer
 	
 	
+	// orientation/motion vars
+	Quaternion q;           // [w, x, y, z]         quaternion container
+	VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+	VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+	VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+	VectorFloat gravity;    // [x, y, z]            gravity vector
+	float euler[3];         // [psi, theta, phi]    Euler angle container
+	float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+	
+	
 	//SerialCommunicator *serCom; // For sending error messages.
 	
 
