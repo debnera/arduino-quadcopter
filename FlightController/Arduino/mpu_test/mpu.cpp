7bb2a6e4c4ea57cb5a39a1ea57f30155dc73feec
Anton Debner <anton.debner@gmail.com>
2015-12-17 18:08:00 +0200

    Restored all hardware serial prints. I don't think they are harmful.

--- FlightController/Arduino/mpu_test/mpu.cpp
+++ FlightController/Arduino/mpu_test/mpu.cpp
@@ -21,24 +21,24 @@ bool MPU::init()
 	#endif
 	
 	// initialize device
-    // Serial.println(F("Initializing I2C devices..."));
+    Serial.println(F("Initializing I2C devices..."));
     mpu.initialize();
 
     // verify connection
-    // Serial.println(F("Testing device connections..."));
-    // Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+    Serial.println(F("Testing device connections..."));
+    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
 	if (mpu.testConnection() == false)
 	{
 		return false;
 	}
     // wait for ready
-    // Serial.println(F("\nSend any character to begin DMP programming and demo: "));
+    Serial.println(F("\nSend any character to begin DMP programming and demo: "));
     while (Serial.available() && Serial.read()); // empty buffer
     // while (!Serial.available());                 // wait for data
     // while (Serial.available() && Serial.read()); // empty buffer again
 
     // load and configure the DMP
-    // Serial.println(F("Initializing DMP..."));
+    Serial.println(F("Initializing DMP..."));
     devStatus = mpu.dmpInitialize();
 
     // supply your own gyro offsets here, scaled for min sensitivity
@@ -50,11 +50,11 @@ bool MPU::init()
     // make sure it worked (returns 0 if so)
     if (devStatus == 0) {
         // turn on the DMP, now that it's ready
-        // Serial.println(F("Enabling DMP..."));
+        Serial.println(F("Enabling DMP..."));
         mpu.setDMPEnabled(true);
 
         // enable Arduino interrupt detection
-        // Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+        Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
 		mpuInterrupt = false;
 		
 		/*
@@ -72,7 +72,7 @@ bool MPU::init()
         mpuIntStatus = mpu.getIntStatus();
 
         // set our DMP Ready flag so the main loop() function knows it's okay to use it
-        // Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        Serial.println(F("DMP ready! Waiting for first interrupt..."));
         dmpReady = true;
 
         // get expected DMP packet size for later comparison
@@ -82,9 +82,9 @@ bool MPU::init()
         // 1 = initial memory load failed
         // 2 = DMP configuration updates failed
         // (if it's going to break, usually the code will be 1)
-        // Serial.print(F("DMP Initialization failed (code "));
-        // Serial.print(devStatus);
-        // Serial.println(F(")"));
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
 		return false;
     }
 	return true;
@@ -104,7 +104,7 @@ Angles MPU::getAngles()
     if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
         // reset so we can continue cleanly
         mpu.resetFIFO();
-        // Serial.println(F("FIFO overflow!"));
+        Serial.println(F("FIFO overflow!"));
 
     // otherwise, check for DMP data ready interrupt (this should happen frequently)
     } 
