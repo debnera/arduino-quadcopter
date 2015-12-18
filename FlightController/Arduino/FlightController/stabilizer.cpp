40859019d75509fa5c34ec80a00c2d66ca98ef67
Anton Debner <anton.debner@gmail.com>
2015-12-18 22:29:21 +0200

    Stabilizer.cpp: Added pid values

--- FlightController/Arduino/FlightController/stabilizer.cpp
+++ FlightController/Arduino/FlightController/stabilizer.cpp
@@ -1,13 +1,36 @@
-// 
-// 
-// 
+//
+//
+//
 
 #include "stabilizer.h"
 
 
 Stabilizer::Stabilizer(int max_motor_power)
 {
-	// TODO - implement me
+	pid_yaw_rate.p = 2.5;
+	//pid_yaw_rate.i = 1;
+	pid_yaw_rate.d = 0;
+	pid_yaw_rate.imax = 50;
+
+	pid_pitch_rate.p = 0.7;
+	//pid_pitch_rate.i = 1;
+	pid_pitch_rate.d = 0;
+	pid_pitch_rate.imax = 50;
+
+	pid_roll_rate.p ) 0.7;
+	//pid_roll_rate.i = 1;
+	pid_roll_rate.d = 0;
+	pid_roll_rate.imax = 50;
+
+	pid_yaw_stab.p = 10;
+	pid_yaw_stab.i = 0;
+	pid_yaw_stab.d = 0;
+	pid_pitch_stab.p = 4.5;
+	pid_pitch_stab.i = 0;
+	pid_pitch_stab.d = 0;
+	pid_roll_stab.p = 4.5;
+	pid_roll_stab.i = 0;
+	pid_roll_stab.d = 0;
 }
 
 Angles Stabilizer::calculateRates(Angles target_angles, Angles cur_angles)
