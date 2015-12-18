650ef61899b5b94eca6780d6cda8f0310d3ca517
Anton Debner <anton.debner@gmail.com>
2015-12-18 21:56:49 +0200

    pid.h: Renamed variables for clarity

--- FlightController/Arduino/FlightController/pid.h
+++ FlightController/Arduino/FlightController/pid.h
@@ -8,15 +8,15 @@ Author:		Anton
 class PID
 {
 public:
-	float k_p;
-	float k_i;
-	float k_d;
+	float p;
+	float i;
+	float d;
 	float imax;
-	PID(float k_p = 0, float k_i = 0, float k_d = 0, float imax = 0)
+	PID(float p = 0, float i = 0, float d = 0, float imax = 0)
 	{
-		this->k_p = k_p;
-		this->k_i = k_i;
-		this->k_d = k_d;
+		this->p = p;
+		this->i = i;
+		this->d = d;
 		this->imax = imax;
 	};
 };
