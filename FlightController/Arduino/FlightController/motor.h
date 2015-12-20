2aa421996d5f56752d70638920b49f85fecff0c4
Anton Debner <anton.debner@gmail.com>
2015-12-21 00:25:17 +0200

    Motor: Removed all pwm related variables. Main program handles the esc pwm range.

--- FlightController/Arduino/FlightController/motor.h
+++ FlightController/Arduino/FlightController/motor.h
@@ -18,19 +18,15 @@ class Motor
 private:
 	Servo servo;
 	int cur_power;
-	int min_pwm_width;
-	int max_pwm_width;
 	String name;
 	int pin;
 
 public:
-	// min_pwm_signal and max_pwm_signal depends on which ESC is used.
-	Motor(int pin, String name, int min_pwm_signal, int max_pwm_signal);
+	Motor(int pin, String name);
 	~Motor();
 	bool attach(); // Attaches the motor - it's ready to be used.
 	bool detach(); // Detaches the motor making it stop.
-	void setPower(int value); // Sets motor power in range [0 , getMaxMotorPower()]
-	int getMaxMotorPower(); // Returns (max_pwm_width - min_pwm_width)
+	void setPower(int value); // Sets motor power in microseconds.
 	String getName();
 };
 
