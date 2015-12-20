33fa1d198946f34ff9b9373653d8d40d0af0a33a
Anton Debner <anton.debner@gmail.com>
2015-12-20 15:25:34 +0200

    Pid: Implemented i and d terms of PID

--- FlightController/Arduino/FlightController/pid.h
+++ FlightController/Arduino/FlightController/pid.h
@@ -7,18 +7,35 @@ Author:		Anton
 
 class PID
 {
+private:
+	float i_sum;
+	float last_error;
+	float d_speed; // Current derivative; or the speed at which error changes
+
+
+
 public:
 	float p;
 	float i;
 	float d;
-	float imax;
+	float imax; // i term won't exceed this value
+	float sample_time; // How often new values are measured?
+
 	PID(float p = 0, float i = 0, float d = 0, float imax = 0)
 	{
 		this->p = p;
-		this->i = i;
-		this->d = d;
+		this->i = i * sample_time/1000;
+		this->d = d / sample_time/1000;
 		this->imax = imax;
+		sample_time = 20; // in milliseconds
 	};
+
+	float calculate(float difference)
+	{
+		i_sum += difference;
+		d_speed = difference - last_error;
+		return difference * p + i_sum * i + d_speed * d;
+	}
 };
 
 #endif
