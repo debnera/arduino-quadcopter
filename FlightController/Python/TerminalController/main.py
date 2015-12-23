74eebd25844b8e6f60e4e9c90a8189d4f342e5c1
Anton Debner <anton.debner@gmail.com>
2015-12-23 12:04:53 +0200

    TerminalController: Simplified for simpler testing

--- FlightController/Python/TerminalController/main.py
+++ FlightController/Python/TerminalController/main.py
@@ -24,34 +24,12 @@ class ListeningThread(Thread):
         maxDelay = 1/5
         while(self.stopRequested == False):
             if (self.ser.inWaiting() > 0):
-                delay = 0
-                incoming = ''
-                # We assume that every incoming message ends in newline ('\n').
-                while(incoming == '' or incoming[-1] != '\n'):
-                    if (self.ser.inWaiting() > 0):
-                        try:
-                            incoming += self.ser.read(1).decode()
-                        except UnicodeDecodeError:
-                            print("UnicodeDecodeError: Received bad data")
-                    elif (maxDelay > delay):
-                        # Lets sleep for a moment in case there is still incoming data
-                        time.sleep(minDelay)
-                        delay += minDelay
-                    else:
-                        # MaxDelay reached, but the incoming string doesn't end with '\n'
-                        print("Warning: maxDelay exceeded when waiting for incoming data. (Does the data end in '\\n'?) \n")
-                        incoming += '\n' # Add '\n' for consistency
-                if (incoming != '' and incoming[0] != '\n'):
-                    self.parseCommand(incoming)
+                try:
+                    print(self.ser.read(1).decode())
+                except UnicodeDecodeError:
+                    print("UnicodeDecodeError: Received bad data")
         print("Stopped listening")
         
-    def parseCommand(self, command):
-        stripped = command.strip().lower()
-        if stripped == self.ConnectionQuery.lower():
-            self.echoPing()
-        else:
-            print("Quad: ", end='') # We want to print without newline for cleaner output.
-            print(command)
             
     def echoPing(self):
         self.ser.write(self.ConnectionAnswer.encode())
